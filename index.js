8db7d37d56c576f64c4a197104752c67b648b514
RHODA NJERI MUYA <njerirhoda31@gmail.com>
2024-03-19 14:20:32 +0300

    functions||

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+//console.log(saturdayFun("Ice-skate"));
+const mondayWork = function(work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+}
+function wrapAdjective(greet = "*") {
+    return function(adjective = "special") {
+        return `You are ${greet}${adjective}${greet}!`
+    }
+}
+//const hello = wrapAdjective('||')('an awesome person');
+//console.log(hello);
